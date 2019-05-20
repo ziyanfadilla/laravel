15b19c1924432f7666c74c8ca3ddf82ffe8fe8be
ziyanfadilla <ziyanfadilla1@gmail.com>
2019-05-20 21:37:12 +0700

    wolcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ziyan Fadilla
                 </div>
 
                 <div class="links">
